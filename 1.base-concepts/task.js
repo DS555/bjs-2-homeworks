365ce04beefce0512ed454233043bc008e155155
D.Sizov <daniil-sizov@yandex.ru>
2023-09-05 23:18:16 +0300

    Решение задачи 1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = ((b**2)-4*a*c);
+  if (d === 0) {
+    let x1 = (-b)/(2*a);
+    arr.push(x1);
+   } 
+   else if (d > 0) {
+    let x1 = ((-b + Math.sqrt(d) )/(2*a));
+    let x2 = ((-b - Math.sqrt(d) )/(2*a));
+    arr.push(x1, x2);
+   }
   return arr;
 }
 
