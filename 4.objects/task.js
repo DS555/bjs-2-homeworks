13b51701872ed2e804c3d7ee5bd348b336e264c9
DS555 <daniil-sizov@yandex.ru>
2024-05-05 22:30:40 +0300

    Четвёртое задание (Основы JS - Объекты) полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,84 +1,48 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-  };
-  
-  Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.hasOwnProperty("marks") === true) {
+    this.marks = (this.marks.map((i) => i), marksToAdd);
   }
-  
-  Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.hasOwnProperty("marks") === true) {
-    this.marks = (this.marks.map(i => i), marksToAdd);
-    }
-  }
-  
-  Student.prototype.exclude = function (reason) {
-    this.excluded = reason;
-    if (this.hasOwnProperty("excluded") === true) {
+};
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  if (this.hasOwnProperty("excluded") === true) {
     delete this.marks;
     delete this.subject;
-    }
   }
-  
-  function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-  };
-  
-  Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-  };
-  
-  Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.hasOwnProperty('marks') === true) {
-      this.marks = (this.marks.map((i) => i), marksToAdd);
-    }
-  };
-  
-  Student.prototype.exclude = function (reason) {
-    this.excluded = reason;
-    if (this.hasOwnProperty('excluded') === true) {
-      delete this.marks;
-      delete this.subject;
-    }
-  };
-  
-  Student.prototype.getAverage = function () {
-    if (this.hasOwnProperty('marks') !== true) {
+};
+
+Student.prototype.getAverage = function () {
+  if (this.hasOwnProperty("marks") !== true) {
     return 0;
-    } 
-    this.average = (this.marks.reduce((acc, item, index, arr) => { acc += item;
-      if (index === arr.length - 1) {
+  }
+  this.average = this.marks.reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
       return acc / arr.length;
-      }
-      return acc;
-      }, 0)
-    )                
-  };
-  
-  let student1 = new Student("Василиса", "женский", 19);
-  student1.setSubject("Algebra");
-  console.log(student1.getAverage());
-  student1.addMarks(4, 5, 4, 5);
-  console.log(student1.getAverage());
-  console.log(student1);
-  let student2 = new Student("Артём", "мужской", 25);
-  student2.setSubject("Geometry");
-  student2.exclude("плохая учёба");
-  console.log(student2);
-  
-  let student1 = new Student("Василиса", "женский", 19);
-  student1.setSubject("Algebra");
-  console.log(student1.getAverage());
-  student1.addMarks(4, 5, 4, 5);
-  console.log(student1.getAverage());
-  console.log(student1);
-  let student2 = new Student("Артём", "мужской", 25);
-  student2.setSubject("Geometry");
-  student2.exclude('плохая учёба');
-  console.log(student2);
+    }
+    return acc;
+  }, 0);
+};
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude("плохая учёба");
+console.log(student2);
