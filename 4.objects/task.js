0dd1431f6d00526af7fab081c735d2becb37432e
DS555 <daniil-sizov@yandex.ru>
2024-05-06 23:17:19 +0300

    Четвёртое задание полностью готово вер.4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,9 +10,6 @@ Student.prototype.setSubject = function (subjectName) {
 };
 
 Student.prototype.addMarks = function (...marksToAdd) {
-  if (!this.hasOwnProperty("marks") === true) {
-    this.marks = (this.marks.map((i) => i), marksToAdd);
-  };
   if (this.hasOwnProperty("marks")) {
     this.marks = [].concat(this.marks, marksToAdd);
   };
